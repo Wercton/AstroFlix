2afa573ce823500e7f1b42a147eda6fbf3fe8c49
Wercton Barbosa <werctonmatheus@gmail.com>
2022-10-20 09:07:39 -0300

    refactor: separando em pastas os arquivos

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:westerosflix/destaque.dart';
-import 'package:westerosflix/personal_appbar.dart';
-import 'package:westerosflix/video_card.dart';
+import 'screens/home_page.dart';
 
 void main() {
   runApp(const MyApp());
@@ -19,29 +17,6 @@ class MyApp extends StatelessWidget {
       ),
       home: const HomePage(),
       debugShowCheckedModeBanner: false,
-    );
-  }
-}
-
-class HomePage extends StatelessWidget {
-  const HomePage({Key? key}) : super(key: key);
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      backgroundColor: const Color.fromARGB(255, 26, 26, 26),
-      body: ListView(
-        children: const [
-          PersonalAppBar("ASTROFLIX"),
-          Destaque(),
-          Video(),
-          Video(),
-          Video(),
-          Video(),
-        ],
-      ),
-      floatingActionButton:
-          FloatingActionButton(onPressed: () {}, child: const Icon(Icons.add)),
     );
   }
 }
---
+++ lib/screens/home_page.dart
@@ -0,0 +1,27 @@
+import 'package:flutter/material.dart';
+import '../components/personal_appbar.dart';
+import '../components/destaque.dart';
+import '../components/video_card.dart';
+
+class HomePage extends StatelessWidget {
+  const HomePage({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: const Color.fromARGB(255, 26, 26, 26),
+      body: ListView(
+        children: const [
+          PersonalAppBar("ASTROFLIX"),
+          Destaque(),
+          Video(),
+          Video(),
+          Video(),
+          Video(),
+        ],
+      ),
+      floatingActionButton:
+          FloatingActionButton(onPressed: () {}, child: const Icon(Icons.add)),
+    );
+  }
+}
